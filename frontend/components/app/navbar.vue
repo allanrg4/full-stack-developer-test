267ee8435ce92fdc4002ad860f3b003828741e0a
Allan Rodriguez <0x.allan@gmail.com>
2024-04-21 22:09:52 -0600

    feat: update navbar design

--- frontend/components/app/navbar.vue
+++ frontend/components/app/navbar.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { UserCircleIcon } from '@heroicons/vue/24/outline'
+
 const auth = useAuthStore()
 
 function onSignOut() {
@@ -8,46 +10,51 @@ function onSignOut() {
 </script>
 
 <template>
-  <div class="navbar bg-base-100">
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl">GES</a>
-    </div>
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="container mx-auto">
+      <div class="flex-1">
+        <img
+          src="https://www.galileo.edu/wp-content/themes/galileo-theme/img/logo-header.png"
+          alt="Images de Universidad Galileo"
+          draggable="false"
+          class="w-20"
+        />
+      </div>
 
-    <div class="flex-none gap-2">
-      <div class="dropdown dropdown-end">
-        <div
-          tabindex="0"
-          role="button"
-          class="avatar btn btn-circle btn-ghost"
-        >
-          <div class="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            />
+      <div class="flex-none gap-2">
+        <div class="dropdown dropdown-end">
+          <div
+            tabindex="0"
+            role="button"
+            class="avatar btn btn-circle btn-ghost"
+          >
+            <UserCircleIcon class="w-8" />
           </div>
-        </div>
 
-        <ul
-          tabindex="0"
-          class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
-        >
-          <li>
-            <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li>
-            <a
-              href="/login"
-              @click="onSignOut"
-            >
-              Logout
-            </a>
-          </li>
-        </ul>
+          <ul
+            tabindex="0"
+            class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+          >
+            <li>
+              <a class="justify-between">
+                Perfil
+                <span class="badge">New</span>
+              </a>
+            </li>
+
+            <li><a>Ajustes</a></li>
+
+            <li>
+              <a
+                href="/login"
+                @click="onSignOut"
+                class="text-error"
+              >
+                Cerrar Sesión
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
