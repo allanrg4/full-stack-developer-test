026a5a80241ac0690d7b369441f3bc80d9add0ae
Allan Rodriguez <0x.allan@gmail.com>
2024-04-21 23:35:42 -0600

    feat: add assignments str

--- backend/assignments/models.py
+++ backend/assignments/models.py
@@ -6,3 +6,6 @@ class Assignment(models.Model):
     session = models.ForeignKey('meetings.Session', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.student} - {self.session}'
